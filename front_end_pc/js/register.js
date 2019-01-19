a1ebe33d8dae18d05c949b3cb51f0606d10a3eed
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2019-01-19 21:59:15 +0800

    发送短信验证码前端准备

--- front_end_pc/js/register.js
+++ front_end_pc/js/register.js
@@ -14,7 +14,9 @@ var vm = new Vue({
 		password2: '',
 		mobile: '', 
 		sms_code: '',
-		allow: false
+		allow: false,
+        sms_code_tip: '获取短信验证码',  // 获取短信验证码将来变成倒计时
+        error_sms_code_message: '',  // 短信错误提示信息
 	},
 	methods: {
 		check_username: function (){
@@ -62,6 +64,56 @@ var vm = new Vue({
 				this.error_allow = false;
 			}
 		},
+		// 发送短信验证码
+        send_sms_code: function () {
+            if (this.sending_flag == true) {
+                return;
+            }
+            this.sending_flag = true;
+
+            // 校验参数，保证输入框有数据填写
+            this.check_phone();
+
+            if (this.error_phone == true) {
+                this.sending_flag = false;
+                return;
+            }
+
+            // 向后端接口发送请求，让后端发送短信验证码
+            axios.get('http://127.0.0.1:8000' + '/sms_codes/' + this.mobile + '/', {
+                responseType: 'json'
+            })
+                .then(response => {
+                    // 表示后端发送短信成功
+                    // 倒计时60秒，60秒后允许用户再次点击发送短信验证码的按钮
+                    var num = 60;
+                    // 设置一个计时器
+                    var t = setInterval(() => {
+                        if (num == 1) {
+                            // 如果计时器到最后, 清除计时器对象
+                            clearInterval(t);
+                            // 将点击获取验证码的按钮展示的文本回复成原始文本
+                            this.sms_code_tip = '获取短信验证码';
+                            // 将点击按钮的onclick事件函数恢复回去
+                            this.sending_flag = false;
+                        } else {
+                            num -= 1;
+                            // 展示倒计时信息
+                            this.sms_code_tip = num + '秒';
+                        }
+                    }, 1000, 60)
+                })
+                .catch(error => {
+                    if (error.status == 400) {
+                        // 展示发送短信错误提示
+                        this.error_sms_code = true;
+                        this.error_sms_code_message = error.data.message;
+                    } else {
+                        console.log(error.data);
+                    }
+                    this.sending_flag = false;
+                })
+        },
 		// 注册
 		on_submit: function(){
 			this.check_username();
--- meiduo_mall/meiduo_mall/apps/verifications/urls.py
+++ meiduo_mall/meiduo_mall/apps/verifications/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     # 获取短信验证码
     # GET /sms_codes/(?P<mobile>1[3-9]\d{9})/
-    url(r'^sms_code/(?P<mobile>1[3-9]\d{9})/$', views.SMSCodeView.as_view())
+    url(r'^sms_codes/(?P<mobile>1[3-9]\d{9})/$', views.SMSCodeView.as_view())
 ]
