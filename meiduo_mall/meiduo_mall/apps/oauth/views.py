dfb5ef940962c2a9031438b32921738a6ea55abb
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2019-01-22 17:12:29 +0800

    QQ扫码后回调处理-已绑定用户直接登录

--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -1,11 +1,64 @@
+import logging
+
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from QQLoginTool.QQtool import OAuthQQ
 from django.conf import settings
+from rest_framework_jwt.settings import api_settings
+
+from .models import QQAuthUser
+
+logger = logging.getLogger('django')
 
 
 # Create your views here.
+class QQAuthUserView(APIView):
+    """扫码成功后回调处理"""
+
+    def get(self, request):
+        # 1.获取查询参数中的code参数
+        code = request.query_params.get('code')
+        if not code:
+            return Response({'message': '缺少code'}, status=status.HTTP_400_BAD_REQUEST)
+        # 1.1 创建qq登录工具对象
+        oauthqq = OAuthQQ(client_id=settings.QQ_CLIENT_ID,
+                          client_secret=settings.QQ_CLIENT_SECRET,
+                          redirect_uri=settings.QQ_REDIRECT_URI,
+                          state=next)
+
+        try:
+            # 2.通过code向QQ服务器请求获取access_token
+            access_token = oauthqq.get_access_token(code)
+            # 3.通过access_token向QQ服务器获取openid
+            openid = oauthqq.get_open_id(access_token)
+        except Exception as e:
+            logger.info(e)
+            return Response({'message': 'QQ服务器异常'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+
+        try:
+            # 4.查询open是否绑定过美多商城中的用户
+            qqauth_model = QQAuthUser.objects.get(openid=openid)
+        except QQAuthUser.DoesNotExist:
+            # 如果openid没有绑定过美多商城中的用户
+            # 把openid进行加密安全处理,再响应给浏览器,让它先帮我们保存一会
+            pass
+        else:
+            # 如果openid已经绑定过美多商城中的用户(生成的jwt token状态保持信息直接让它登录成功)
+            # 手动生成token
+            jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER  # 加载生成载荷函数
+            jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER  # 加载生成token函数
+            # 获取user对象
+            user = qqauth_model.user
+            payload = jwt_payload_handler(user)  # 生成载荷
+            token = jwt_encode_handler(payload)  # 根据载荷生成token
+
+            return Response({
+                'token': token,
+                'username': user.username,
+                'user_id': user.id
+            })
 
 
 class QQAuthURLView(APIView):
