2294f7f3b0c214a80f55b37d68864889205de1d2
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2019-02-01 23:21:23 +0800

    商品列表界面静态化定义异步任务

--- meiduo_mall/celery_tasks/main.py
+++ meiduo_mall/celery_tasks/main.py
@@ -13,4 +13,4 @@ celery_app = Celery('meiduo_mall')
 celery_app.config_from_object('celery_tasks.config')
 
 # 3.注册异步任务(哪些任务可以进入到任务队列)
-celery_app.autodiscover_tasks(['celery_tasks.sms', 'celery_tasks.email'])
+celery_app.autodiscover_tasks(['celery_tasks.sms', 'celery_tasks.email', 'celery_tasks.html'])
