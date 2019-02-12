85b334d8158903ae6000818ad41575de686f81a0
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2019-02-12 22:22:26 +0800

    解决认证问题

--- meiduo_mall/meiduo_mall/apps/carts/views.py
+++ meiduo_mall/meiduo_mall/apps/carts/views.py
@@ -8,9 +8,18 @@ from rest_framework.views import APIView
 class CartView(APIView):
     """购物车视图"""
 
+    def perform_authentication(self, request):
+        """禁用认证/延后认证"""
+        pass
+
     def post(self, request):
         """添加购物车"""
-        pass
+        try:
+            user = request.user  # 获取登录用户  首次获取还会做认证
+            # 如果代码能继续向下走说明是登录用户, 存储购物车数据到redis
+        except:
+            # 未登录用户存储到cookie
+            pass
 
     def get(self, request):
         """查询购物车"""
