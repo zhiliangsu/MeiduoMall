c8499c8615271a435c4175d91d4ecc6acd35b0d5
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2019-02-13 16:57:39 +0800

    定义添加购物车序列化器

---
+++ meiduo_mall/meiduo_mall/apps/carts/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from goods.models import SKU
+
+
+class CartSerializer(serializers.Serializer):
+    """购物车序列化器: 校验数据使用"""
+
+    sku_id = serializers.IntegerField(label='商品sku_id', min_value=1)
+    count = serializers.IntegerField(label='商品数量', min_value=1)
+    selected = serializers.BooleanField(label='勾选状态', default=True)
+
+    def validate_sku_id(self, value):
+        """校验sku_id"""
+
+        try:
+            SKU.objects.get(id=value)
+        except SKU.DoesNotExist:
+            raise serializers.ValidationError('商品sku_id不存在')
+
+        return value
