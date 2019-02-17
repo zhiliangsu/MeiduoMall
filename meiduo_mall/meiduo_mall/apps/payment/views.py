40c70d9894a53ddf20e4268b27a537614ad63588
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2019-02-17 20:09:59 +0800

    支付成功验证并存储交易号和订单号及修改订单状态

--- meiduo_mall/meiduo_mall/apps/payment/views.py
+++ meiduo_mall/meiduo_mall/apps/payment/views.py
@@ -8,6 +8,7 @@ from rest_framework.permissions import IsAuthenticated
 import os
 
 from orders.models import OrderInfo
+from .models import Payment
 
 
 # Create your views here.
@@ -48,8 +49,8 @@ class PaymentView(APIView):
         # 电脑网站支付，需要跳转到https://openapi.alipaydev.com/gateway.do? + order_string
         order_string = alipay.api_alipay_trade_page_pay(
             out_trade_no=order_id,  # 美多商城订单编号
-            total_amount=str(order.total_amount),  # 一定要把总价转换成字符串类型,不然会报错
-            subject='美多商城%s' % order_id,
+            total_amount=str(order.total_amount),  # 注意: 一定要把总价转换成字符串类型,不然会报错
+            subject='美多商城%s' % order_id,  # 注意: 如果变量是字符串类型必须用%s占位,如果是int类型可以用%d也可以用%s
             return_url="http://www.meiduo.site:8080/pay_success.html"  # 支付成功后回调的url
         )
         # 拼接完整的支付宝登录url
@@ -65,9 +66,40 @@ class PaymentStatusView(APIView):
 
     def put(self, request):
         # 获取查询参数中的所有数据 query_params / django GET
-        # 把查询参数中的sign签名部分数据单独提取出
-        # 调用alipay中的verify方法进行验证支付信息
+        query_dict = request.query_params
+        # 把query_dict类型的对象转换成字典
+        data = query_dict.dict()
 
-        # 把交易号和订单编号进行绑定存储
-        # 修改订单状态
-        pass
+        # 把查询参数中的sign签名部分数据单独提取出
+        sign = data.pop('sign')
+
+        # 创建alipay支付对象
+        alipay = AliPay(
+            appid=settings.ALIPAY_APPID,
+            app_notify_url=None,  # 默认回调url
+            app_private_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'keys/app_private_key.pem'),
+            alipay_public_key_path=os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                                'keys/alipay_public_key.pem'),
+            sign_type="RSA2",  # RSA 或者 RSA2
+            debug=settings.ALIPAY_DEBUG  # 默认False
+        )
+        # 调用alipay中的verify方法进行验证支付信息
+        success = alipay.verify(data, sign)
+
+        if success:
+            # 获取美多订单号
+            order_id = data.get('out_trade_no')
+            # 获取支付宝交易流水号
+            trade_id = data.get('trade_no')
+            # 把交易号和订单编号进行绑定存储
+            Payment.objects.create(
+                order_id=order_id,
+                trade_id=trade_id
+            )
+            # 修改订单状态
+            OrderInfo.objects.filter(order_id=order_id, status=OrderInfo.ORDER_STATUS_ENUM['UNPAID']).update(
+                status=OrderInfo.ORDER_STATUS_ENUM['UNSEND'])
+
+            return Response({'trade_id': trade_id})
+        else:
+            return Response({'message': '非法请求'}, status=status.HTTP_403_FORBIDDEN)
